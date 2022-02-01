3a252d6eb4728d16631ab4c4eeaceb0699b87cf0
Umang Thanki <u_thanki@yahoo.com>
2022-01-31 22:35:08 -0600

    Initial Commit for Project

---
+++ coffee_machine.py
@@ -0,0 +1,27 @@
+from coffee_maker import CoffeeMaker
+from menu import Menu, MenuItem
+from money_machine import MoneyMachine
+
+coff_mach = CoffeeMaker()
+coff_mach_menu = Menu()  # Already has all the drink information added.
+
+bank = MoneyMachine()
+
+continue_running = True
+
+while continue_running:
+    choice = input("What would you like to do? (items/espresso/latte/cappuccino/report/off): ")
+    if choice == "off":
+        continue_running = False
+        print("Machine Shutdown")
+    elif choice == "items":
+        print(coff_mach_menu.get_items())
+    elif choice == "report":
+        coff_mach.report()
+        bank.report()
+    elif choice == "espresso" or choice == "latte" or choice == "cappuccino":
+        for drink in coff_mach_menu.menu:
+            if drink.name == choice:
+                if coff_mach.is_resource_sufficient(drink):
+                    bank.make_payment(drink.cost)
+                    coff_mach.make_coffee(drink)
